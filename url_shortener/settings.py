3e6d53e73b2a4036fa27c39be5e7b0804b0b3c71
Paul utti <108591280+Tegzypro@users.noreply.github.com>
2023-07-09 17:04:19 +0100

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -5,4 +5,3 @@ SQLALCHEMY_DATABASE_URI='sqlite:///db.sqlite3'
 SQLALCHEMY_TRACK_NOTIFICATIONS = False
 ADMIN_USERNAME=os.environ.get('ADMIN-USERNAME')
 ADMIN_PASSWORD=os.environ.get('ADMIN_PASSWORD')
-# postgres://scissors_shortener_user:U3emawxW6tPTUEXwtPGxl0qbiJCFSSq7@dpg-cihm3ulgkuvojja275e0-a.oregon-postgres.render.com/scissors_shortener
